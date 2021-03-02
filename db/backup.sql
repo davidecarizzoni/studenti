104ffacd20ca23616cc38bd36b473cae76c3395f
Riccardo-2000 <you@example.comrizzini.riccardo.marconi@gmail.com>
2021-03-02 16:26:05 +0100

    chiave esterna

--- db/backup.sql
+++ db/backup.sql
@@ -2,8 +2,8 @@
 --
 -- Host: localhost    Database: studentdb
 -- ------------------------------------------------------
--- Server version	5.7.33
-
+-- Server version 5.7.33
+ 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -14,11 +14,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+ 
 --
 -- Table structure for table `Student`
 --
-
+ 
 DROP TABLE IF EXISTS `Student`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -26,22 +26,24 @@ CREATE TABLE `Student` (
   `Id` int NOT NULL,
   `Name` varchar(10) NOT NULL,
   `Surname` varchar(12) NOT NULL,
-  
-  PRIMARY KEY (`Id`)
+  `IdRoom` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  FOREIGN KEY (`IdRoom`) REFERENCES Room (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
+ 
 --
 -- Dumping data for table `Student`
 --
-
+ 
 LOCK TABLES `Student` WRITE;
 /*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-INSERT INTO `Student` VALUES (1,'ola','ola');
+INSERT INTO `Student` VALUES (1,'Studente1','Studente1',1),
+                             (2,'Studente2','Studente2',1);
 UNLOCK TABLES;
 
 
-
+ 
 DROP TABLE IF EXISTS `Room`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -53,23 +55,21 @@ CREATE TABLE `Room` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
+ 
 --
 -- Dumping data for table `Room`
 --
-
+ 
 LOCK TABLES `Room` WRITE;
 /*!40000 ALTER TABLE `Room` DISABLE KEYS */;
 INSERT INTO `Room` VALUES (1,'Aula 1',20),
                           (2,'Aula 2',20);
 UNLOCK TABLES;
-
-
+ 
 DROP TABLE IF EXISTS `Course`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-
-
+ 
 CREATE TABLE `Course` (
   `Id` int NOT NULL,
   `Name` varchar(20) NOT NULL,
@@ -77,15 +77,14 @@ CREATE TABLE `Course` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
+ 
 --
 -- Dumping data for table `Course`
 --
-
+ 
 LOCK TABLES `Course` WRITE;
 /*!40000 ALTER TABLE `Course` DISABLE KEYS */;
 INSERT INTO `Course` VALUES (1,'Matematica'),
-                           (2,'Italiano'),
-                           (3,'Donati');
+                            (2,'Italiano'  ),
+                            (3,'Donati'    );
 UNLOCK TABLES;
-
